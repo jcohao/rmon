1cb024ee8430ec03450d46273e355171ccd42972
jcohao <283906175@qq.com>
2018-09-10 17:19:38 +0800

    添加测试代码

---
+++ tests/test_models.py
@@ -0,0 +1,30 @@
+from rmon.models import Server
+
+class TestServer:
+    """
+        test Server's functions
+    """
+
+    def test_save(self, db):
+        """
+            test Server.save
+        """
+        # 初始状态下数据库没有保存Redis，所以此时数量为0
+
+        assert Server.query.count() == 0
+        server = Server(name='test', host='127.0.0.1')
+        server.save()
+        # 现在数据库的数量为1
+        assert Server.query.count() == 1
+        # 且数据库的记录就是之前创建的记录
+        assert Server.query.first() == server
+
+    def test_delete(self, db, server):
+        """
+            test Server.delete
+        """
+
+        assert Server.query.count() == 1
+        server.delete()
+        assert Server.query.count() == 0
+
